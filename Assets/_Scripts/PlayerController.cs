995cf08a8aaa50f743c5ffa9de834463f4b22356
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-05-06 18:11:39 -0300

    feat: adding vegetation

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
     float _baseSpeed = 10.0f;
-    float _gravidade = 9.8f;
+    float _runSpeed = 20.0f;
+    float _actualSpeed = 10.0f;
+    float _gravidade = 2.0f;
+    bool _runnnig = false; 
+    float velJump = 0.0f;
     CharacterController characterController;
     GameObject playerCamera;
     float cameraRotation;
@@ -27,10 +31,11 @@ public class PlayerController : MonoBehaviour{
         cameraRotation = Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
 
         float y = 0;
-        if(!characterController.isGrounded) y = -_gravidade;
+        y += Jump();
+        if(!characterController.isGrounded) y -= _gravidade;
 
         Vector3 direction = transform.right * x + transform.up * y + transform.forward * z;
-        characterController.Move(direction * _baseSpeed * Time.deltaTime);
+        characterController.Move(direction * _actualSpeed * Time.deltaTime);
         transform.Rotate(Vector3.up, mouse_dX);
         playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
     }
@@ -39,7 +44,28 @@ public class PlayerController : MonoBehaviour{
         RaycastHit hit;
         Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.forward*10.0f, Color.magenta);
         if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, 100.0f)){
-            Debug.Log(hit.collider.name);
+            //Debug.Log(hit.collider.name);
+        }
+    }
+
+
+    float Jump(){
+        
+        if(Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded) velJump = 5.0f;
+        velJump -= velJump > 0 ? 7.0f * Time.deltaTime : 0.0f;
+        return velJump;
+    }
+
+    void CheckRun(){
+        print(Input.GetKeyDown(KeyCode.LeftShift));
+        print(_runnnig);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _runnnig){ 
+            _actualSpeed = _baseSpeed;
+            _runnnig = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !_runnnig) {
+            _actualSpeed = _runSpeed;
+            _runnnig = true;
         }
     }
 }
